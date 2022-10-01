a9afbe1a7b63ca311d0ab3d0910a45bc218c5516
Gonzalo Gallardo <gonzalo94.jobs@gmail.com>
2022-10-01 17:03:02 -0300

    agregue cambios en el codigo

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -3,32 +3,29 @@ import { Form, Button } from "react-bootstrap";
 import { useState, useEffect } from "react";
 
 const FormularioTarea = () => {
-  //busco los datos del localstorage
-  const tareasLocalStorage = JSON.parse(localStorage.getItem('arregloTareaKey')) || [];
-  //aqui va la mayoria de la logica
+  const tareasLocalStorage =
+    JSON.parse(localStorage.getItem("arregloTareaKey")) || [];
+
   const [tarea, setTarea] = useState("");
   const [arregloTarea, setArregloTarea] = useState(tareasLocalStorage);
 
-  //ciclo de vida del componente
-  useEffect(()=>{
-    // console.log('prueba de ciclo de vida del componente')
-    //guardar en localstorage
-    localStorage.setItem('arregloTareaKey', JSON.stringify(arregloTarea));
-  },[arregloTarea])
+  useEffect(() => {
+    localStorage.setItem("arregloTareaKey", JSON.stringify(arregloTarea));
+  }, [arregloTarea]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // arregloTarea.push no podemos usar el push con el state
+
     setArregloTarea([...arregloTarea, tarea]);
-    //limpiar el input
-    setTarea('');
+
+    setTarea("");
   };
 
-  const borrarTarea = (nombre)=>{
-    let arregloModificado = arregloTarea.filter((item)=>(item !== nombre ));
-    //actualizo el state
-    setArregloTarea(arregloModificado)
-  }
+  const borrarTarea = (nombre) => {
+    let arregloModificado = arregloTarea.filter((item) => item !== nombre);
+
+    setArregloTarea(arregloModificado);
+  };
 
   return (
     <div>
@@ -46,7 +43,10 @@ const FormularioTarea = () => {
         </Form.Group>
       </Form>
 
-      <ListaTarea arregloTarea={arregloTarea} borrarTarea={borrarTarea}></ListaTarea>
+      <ListaTarea
+        arregloTarea={arregloTarea}
+        borrarTarea={borrarTarea}
+      ></ListaTarea>
     </div>
   );
 };
